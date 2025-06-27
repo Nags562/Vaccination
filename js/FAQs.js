ce1d603c79b07fe3c07b19bd61507f56c78918f0
Kevina <kevinanakayiza@gmail.com>
2025-06-27 14:02:39 +0300

    Have used CSS variables to intiate my colors and customized my JS

--- js/FAQs.js
+++ js/FAQs.js
@@ -25,12 +25,19 @@ articles.forEach(article => {
   const text = article.querySelector(".text");
   const qa = article.querySelector(".qa");
 
+  //Introducing my CSS variables in JS
+  const rootStyles = getComputedStyle(document.documentElement);
+  const coral = rootStyles.getPropertyValue('--color-light-coral').trim();
+  const white = rootStyles.getPropertyValue('--color-white').trim();
+  const concrete = rootStyles.getPropertyValue('--color-concrete').trim();
+
   close.addEventListener("click", () => {
     text.classList.remove("hidden");
     open.classList.remove("hidden");
     close.classList.add("hidden");
-    article.style.backgroundColor = "#EF6B6E";
-    qa.style.color = "#FFFFFF";
+    article.style.backgroundColor = coral;
+    qa.style.color = white;
+    text.style.color = white;
     // svg.forEach(svg => svg.style.color = "FFFFFF");
   });
 
@@ -38,8 +45,8 @@ articles.forEach(article => {
     text.classList.add("hidden");
     close.classList.remove("hidden");
     open.classList.add("hidden");
-    article.style.backgroundColor = "#F1F1F1";
-    qa.style.color = "#000000";
+    article.style.backgroundColor = concrete;
+    qa.style.color = "#888888";
     // svg.forEach(svg => svg.style.color = "F1F1F1");
   });
 
